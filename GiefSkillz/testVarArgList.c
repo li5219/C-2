58699393d6d1aa0d1a5b602d432bfe6c7fa3fca3
Nisse Smorkster <smorkster@gmail.com>
2011-09-25 14:46:57 +0200

    More testing of C functionality

--- GiefSkillz/testVarArgList.c
+++ GiefSkillz/testVarArgList.c
@@ -1,23 +1,30 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-void printArgs (int n_args, ...)
+float
+average (int n_args, ...)
 {
-  int i=0;
-  va_list argList;
-  va_start (argList, n_args);
-  
-  fprintf(stdout, "%d arguments sent to the function.\n", n_args);
-  while (i < n_args)
-   {
-     fprintf(stdout, "Argument %d is %d\n", i, va_arg (argList, int));
-     i += 1;
-   }
+  va_list myList;
+  va_start (myList, n_args);
+
+  int numbersAdded = 0;
+  int sum = 0;
+
+  while (numbersAdded < n_args)
+    {
+      int number = va_arg (myList, int);	// Get next number from list
+      sum += number;
+      numbersAdded += 1;
+    }
+
+  float avg = (float) (sum) / (float) (numbersAdded);	// Find the average
+  return avg;
 }
 
-int main ()
+int
+main ()
 {
-  printArgs(1, 2, 3, 4, 5);
-  
+  print (1, 2, 3, 4, 5);
+
   return 0;
 }
